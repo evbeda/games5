6f384880ad6b6250436fda5704ba14d02e745d84
Olivera Luciano <lolivera@eventbrite.com>
2021-03-15 12:56:29 -0300

    add row class with lock row

--- qwixx/row.py
+++ qwixx/row.py
@@ -1,7 +1,10 @@
 class Row:
     def __init__(self, values, color):
         self.row = values
-        self.color = color 
+        self.color = color
+        self.state = False
+        self.mark = 0
 
-    def lock_Row():
-        raise NotImplementedError
+    def lock_Row(self):
+        self.state = True
+        return True
