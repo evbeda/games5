c781e17e9888ef67b03c8a0b184e6e99149aeeb5
Olivera Luciano <lolivera@eventbrite.com>
2021-03-15 15:10:28 -0300

    fix spaces and import

--- qwixx/test_score_pad.py
+++ qwixx/test_score_pad.py
@@ -1,7 +1,5 @@
 import unittest
-from unittest.mock import patch
 from score_pad import ScorePad
-from game import Game
 
 
 class TestScorePad(unittest.TestCase):
@@ -10,7 +8,7 @@ class TestScorePad(unittest.TestCase):
 
     def test_add_penalty(self):
         self.assertEqual(self.scorepad.add_penalty(), 1)
-    
+
     def test_calculate_score(self):
         self.assertEqual(ScorePad.calculate_score(), 38)
 
