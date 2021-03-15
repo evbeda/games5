fba9de02518fd8b945bf5ea4cfd9ea4fe06c99a9
Olivera Luciano <lolivera@eventbrite.com>
2021-03-15 15:06:28 -0300

    add method  add_penalty

--- qwixx/score_pad.py
+++ qwixx/score_pad.py
@@ -3,4 +3,11 @@ class ScorePad:
         self.penalty = 0
 
     def add_penalty(self):
+        self.penalty += 1
+        if self.penalty < 4:
+            return self.penalty
+        else:
+            return True
+
+    def calculate_score(self):
         raise NotImplementedError
