fa726e768a91f0195682c7e40c85eb94c772c919
Andres Bonilla <abonilla@eventbrite.com>
2021-03-15 13:24:13 -0300

    Add dice set attribute to class

--- qwixx/game.py
+++ qwixx/game.py
@@ -5,6 +5,7 @@ class Game:
     def __init__(self, player_amount=1):
         self.player_amount = player_amount
         self.current_player = player.Player()
+        self.dice_set = []
 
     def create_dice_set(self):
         raise NotImplementedError
