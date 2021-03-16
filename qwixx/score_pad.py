65047b45a6c7cbbd12527969b546a240fb840e14
brz <ibrizuela@eventbrite.com>
2021-03-16 13:58:49 -0300

    tests fixed by nacho and sergio

--- qwixx/score_pad.py
+++ qwixx/score_pad.py
@@ -1,6 +1,17 @@
+from row import Row
+
+
 class ScorePad:
     def __init__(self):
+        self.rows = self.create_rows()
         self.penalty = 0
+
+    def create_rows(self):
+        all_rows = []
+        for color in ['red', 'yellow', 'blue', 'green']:
+            all_rows.append(Row(color))
+
+        return all_rows
 
     def add_penalty(self):
         self.penalty += 1
