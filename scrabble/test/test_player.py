1043672c8f3ff315240a945495cf78d85478ca5c
Enzo Crespillo <ecrespillo@eventbrite.com>
2021-03-16 14:50:57 -0300

    draw full hand and test

--- scrabble/test/test_player.py
+++ scrabble/test/test_player.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import patch
 from ..game.player import Player
 
 
@@ -12,7 +13,7 @@ class TestPlayer(unittest.TestCase):
         self.assertEqual(self.player_test.id, self.id_test)
         self.assertEqual(self.player_test.name, self.name_test)
 
-    def test_draw(self):
+    def test_one_draw(self):
         # data
         tiles_test = [1, 2, 3]
 
@@ -20,6 +21,28 @@ class TestPlayer(unittest.TestCase):
         self.assertEqual(len(self.player_test.tiles_in_hand), 0)
         self.assertEqual(len(tiles_test), 3)
 
-        self.player_test.draw(tiles_test)
+        self.player_test.one_draw(tiles_test)
         self.assertEqual(len(self.player_test.tiles_in_hand), 1)
         self.assertEqual(len(tiles_test), 2)
+
+    @patch('random.randint', return_value=5)
+    def test_one_draw_random_module(self, mock_random):
+        tiles_test = list(range(0, 10))
+        self.player_test.one_draw(tiles_test)
+
+        start = 0
+        end = len(tiles_test)
+        mock_random.assert_called_once_with(start, end)
+
+    def test_draw_full_hand(self):
+        # data
+        tiles_test = list(range(0, 20))
+        self.player_test.tiles_in_hand = [9, 99]
+
+        # process
+        self.assertEqual(len(self.player_test.tiles_in_hand), 2)
+        self.assertEqual(len(tiles_test), 20)
+
+        self.player_test.complete_hand_draw(tiles_test)
+        self.assertEqual(len(self.player_test.tiles_in_hand), 7)
+        self.assertEqual(len(tiles_test), 15)
