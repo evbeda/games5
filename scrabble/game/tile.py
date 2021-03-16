3d224bb540c3fcfa0628ecacab40eb7bb6d71064
Andres Bonilla <abonilla@eventbrite.com>
2021-03-16 11:07:52 -0300

    Add dict for letter scores

--- scrabble/game/tile.py
+++ scrabble/game/tile.py
@@ -1,4 +1,17 @@
 class Tile:
-    def __init__(self, letter, score):
+
+    letter_score = {
+        1: 'aeoisnlrut'.split(),
+        2: 'dg'.split(),
+        3: 'cbmp'.split(),
+        4: 'hfvy'.split(),
+        5: 'ch-q'.split('-'),
+        8: 'j-ll-ñ-rr-x'.split('-'),
+        10: 'z'.split(),
+    }
+
+    def __init__(self, letter):
         self.letter = letter
-        self.score = score
+        for score, letter_list in Tile.letter_score.items():
+            if letter in letter_list:
+                self.score = score
