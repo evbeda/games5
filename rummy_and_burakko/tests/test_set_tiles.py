bc25036e1bb3cdc22ff086c5594a1169081ab302
vgatica-eb <vgatica@eventbrite.com>
2021-03-16 10:50:55 -0300

    coments

--- rummy_and_burakko/tests/test_set_tiles.py
+++ rummy_and_burakko/tests/test_set_tiles.py
@@ -6,7 +6,7 @@ from parameterized import parameterized
 
 
 class TestSetTiles(unittest.TestCase):
-    # Procedure test
+    # Procedure test to identify a leg
     @patch.object(SetTiles, 'is_a_leg', return_value=True)   # 2 argument
     @patch.object(SetTiles, 'is_a_stair', return_value=False)  # 1 argument
     def test_valid_T_F(self, mock_stair, mock_leg):
@@ -18,7 +18,7 @@ class TestSetTiles(unittest.TestCase):
         mock_leg.assert_called_once_with()
         mock_stair.assert_not_called()
 
-    # Procedure test
+    # Procedure test to identify a stair
     @patch.object(SetTiles, 'is_a_leg', return_value=False)   # 2 argument
     @patch.object(SetTiles, 'is_a_stair', return_value=True)  # 1 argument
     def test_valid_F_T(self, mock_stair, mock_leg):
@@ -30,6 +30,7 @@ class TestSetTiles(unittest.TestCase):
         mock_leg.assert_called_once_with()
         mock_stair.assert_called_once_with()
 
+    # Procedure test to dischard leg and stair
     @patch.object(SetTiles, 'is_a_leg', return_value=False)
     @patch.object(SetTiles, 'is_a_stair', return_value=False)
     def test_valid_F_F(self, mock_stair, mock_leg):
@@ -41,6 +42,23 @@ class TestSetTiles(unittest.TestCase):
         mock_leg.assert_called_once_with()
         mock_stair.assert_called_once_with()
 
+    # Procedure test to approve leg
+    @parameterized.expand([
+        (True, (('red', 5), ('blue', 5), ('green', 5))),
+        (False, (('red', 5), ('blue', 6), ('green', 5))),
+        (False, (('red', 5), ('blue', 5), ('blue', 5))),
+        (True, (('*', 0), ('blue', 5), ('green', 5))),
+        (False, (('*', 0), ('blue', 5), ('green', 5), ('*', 0))),
+    ])
+    def test_is_a_stair(self, expected, tiles):
+        # set variables
+        tiles_stair = SetTiles([Tile(t[0], t[1]) for t in tiles])
+        result = tiles_stair.is_a_leg()
+        
+        # assert
+        self.assertEqual(result, expected)
+
+    # Pocedure test to approve a stair
     @parameterized.expand([
         (True, (('red', 5), ('blue', 5), ('green', 5))),
         (False, (('red', 5), ('blue', 6), ('green', 5))),
