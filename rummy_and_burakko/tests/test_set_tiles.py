84e681914c706723dd643d240647de9e6673106a
Olivera Luciano <lolivera@eventbrite.com>
2021-03-16 14:49:38 -0300

    add method is_a_stair and test for is_a_stair

--- rummy_and_burakko/tests/test_set_tiles.py
+++ rummy_and_burakko/tests/test_set_tiles.py
@@ -42,29 +42,36 @@ class TestSetTiles(unittest.TestCase):
         mock_leg.assert_called_once_with()
         mock_stair.assert_called_once_with()
 
-    # Procedure test to approve leg
+    # Procedure test to approve stair
     @parameterized.expand([
-        (True, (('red', 5), ('blue', 5), ('green', 5))),
+        (True, (('red', 5), ('red', 6), ('red', 7))),
         (False, (('red', 5), ('blue', 6), ('green', 5))),
-        (False, (('red', 5), ('blue', 5), ('blue', 5))),
-        (True, (('*', 0), ('blue', 5), ('green', 5))),
+        (True, (('blue', 1), ('blue', 2), ('blue', 3))),
+        (True, (('*', 0), ('blue', 5), ('blue', 6))),
         (False, (('*', 0), ('blue', 5), ('green', 5), ('*', 0))),
+        (False, (('red', 5), ('red', 6))),
+        (False, (('red', 5), ('red', 6), ('red', 7), ('red', 5), ('red', 6),
+                 ('red', 7), ('red', 5), ('red', 6), ('red', 7), ('red', 5),
+                 ('red', 6), ('red', 7), ('blue', 6))),
     ])
     def test_is_a_stair(self, expected, tiles):
         # set variables
         tiles_stair = SetTiles([Tile(t[0], t[1]) for t in tiles])
-        result = tiles_stair.is_a_leg()
+        result = tiles_stair.is_a_stair()
         
         # assert
         self.assertEqual(result, expected)
 
-    # Pocedure test to approve a stair
+    # Pocedure test to approve a leg
     @parameterized.expand([
         (True, (('red', 5), ('blue', 5), ('green', 5))),
         (False, (('red', 5), ('blue', 6), ('green', 5))),
         (False, (('red', 5), ('blue', 5), ('blue', 5))),
         (True, (('*', 0), ('blue', 5), ('green', 5))),
         (False, (('*', 0), ('blue', 5), ('green', 5), ('*', 0))),
+        (False, (('blue', 5), ('green', 5))),
+        (True, (('*', 0), ('blue', 5), ('green', 5), ('red', 5))),
+        (False, (('*', 0), ('blue', 5), ('green', 5), ('red', 5), ('red', 5))),
     ])
     def test_is_a_leg(self, expected, tiles):
         # set variables
