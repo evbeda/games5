fc11d72eea44fb964d916cab5b99770c79817071
brz <ibrizuela@eventbrite.com>
2021-03-16 15:20:12 -0300

    rewrite some tests

--- qwixx/player.py
+++ qwixx/player.py
@@ -1,5 +1,5 @@
 class Player:
     def __init__(self):
-        self.board = [row1, ro2, row3, row4]
+        self.board = []#[row1, ro2, row3, row4]
     def mark_number(self, number, row_color):
         pass
