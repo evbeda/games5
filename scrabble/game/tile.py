c0eed413a77a7ceda37e1087b8f65deca9dd52e4
Andres Bonilla <abonilla@eventbrite.com>
2021-03-16 11:59:10 -0300

    Fix tile letter-score dict

--- scrabble/game/tile.py
+++ scrabble/game/tile.py
@@ -1,17 +1,19 @@
 class Tile:
 
     letter_score = {
-        1: 'aeoisnlrut'.split(),
-        2: 'dg'.split(),
-        3: 'cbmp'.split(),
-        4: 'hfvy'.split(),
+        1: list('aeoisnlrut'),
+        2: list('dg'),
+        3: list('cbmp'),
+        4: list('hfvy'),
         5: 'ch-q'.split('-'),
         8: 'j-ll-ñ-rr-x'.split('-'),
-        10: 'z'.split(),
+        10: list('z'),
     }
 
     def __init__(self, letter):
         self.letter = letter
+        self.score = 0
         for score, letter_list in Tile.letter_score.items():
             if letter in letter_list:
                 self.score = score
+                break
