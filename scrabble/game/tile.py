a2f2a1dcdb959c3526925bbafb92376644699ba3
Olivera Luciano <lolivera@eventbrite.com>
2021-03-17 11:27:09 -0300

    refactoring for class tile

--- scrabble/game/tile.py
+++ scrabble/game/tile.py
@@ -1,23 +1,22 @@
 class Tile:
 
-    letter_score = {
-        1: list('aeoisnlrut'),
-        2: list('dg'),
-        3: list('cbmp'),
-        4: list('hfvy'),
-        5: 'ch-q'.split('-'),
-        8: 'j-ll-ñ-rr-x'.split('-'),
-        10: list('z'),
-    }
+    set_tile = (('aeoisnlrut', 1),
+                ('dg', 2),
+                ('cbmp', 3),
+                ('hfvy', 4),
+                ('ch-q'.split('-'), 5),
+                ('j-ll-ñ-rr-x'.split('-'), 8),
+                ('z', 10)
+                )
 
     def __init__(self, letter):
         self.letter = letter
         self.score = 0
-        for score, letter_list in Tile.letter_score.items():
-            if letter in letter_list:
-                self.score = score
+        for t in Tile.set_tile:
+            if letter in t[0]:
+                self.score = t[1]
                 break
-
+       
     @property
     def order(self):
         return ord(self.letter) - ord('a')
