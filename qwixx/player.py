b06650bc3f66203f97ac477099a5b69bf54aa2f6
Enzo Crespillo <ecrespillo@eventbrite.com>
2021-03-17 17:30:53 -0300

    test de mark y pass number

--- qwixx/player.py
+++ qwixx/player.py
@@ -1,14 +1,18 @@
-# from .score_pad import ScorePad
+from .score_pad import ScorePad
 
 
 class Player:
     def __init__(self):
-        self.board = []  # [row1, ro2, row3, row4]
+        # self.board = []  # [row1, ro2, row3, row4]
         # self.name = name
-        # self.score_pad = ScorePad()
+        self.score_pad = ScorePad()
 
-    def mark_number(self, number, row_color):
-        pass
+    def mark_number(self, number, color):
+        for row in self.score_pad.rows:
+            if row.color != color:
+                continue
+            row.set_marks(number)
+            break
 
     def pass_turn(self):
-        pass
+        return self.score_pad.add_penalty()
