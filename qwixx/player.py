16910466fe73be16bed067746824830eeb5fc79b
Enzo Crespillo <ecrespillo@eventbrite.com>
2021-03-17 11:32:19 -0300

    set_dices and test

--- qwixx/player.py
+++ qwixx/player.py
@@ -1,5 +1,14 @@
+# from .score_pad import ScorePad
+
+
 class Player:
     def __init__(self):
         self.board = []#[row1, ro2, row3, row4]
+        # self.name = name
+        # self.score_pad = ScorePad() 
+
     def mark_number(self, number, row_color):
+        pass
+
+    def pass_turn(self):
         pass
---
+++ qwixx/test_player.py
@@ -0,0 +1,13 @@
+import unittest
+from unittest.mock import patch
+from .player import Player
+
+
+class TestPlayer(unittest.TestCase):
+    def setUp(self):
+        self.t_player = Player()
+        # self.t_player = Player('Test_name')
+
+    # def test_constructor(self):
+        # self.assertEqual(self.t_player.name, 'Test_name')
+
