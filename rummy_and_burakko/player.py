87c5ca19944b21a16a81e639f5994df2aeb2ae52
Andres Bonilla <abonilla@eventbrite.com>
2021-03-17 11:52:41 -0300

    Check and remove tiles from player hand

--- rummy_and_burakko/player.py
+++ rummy_and_burakko/player.py
@@ -3,3 +3,13 @@ class Player:
         self.name = name
         self.first_move = True
         self.hand = []
+
+    def add_tiles(self, tiles):
+        self.hand += tiles
+
+    def remove_tiles(self, tiles):
+        for tile in tiles:
+            if tile in self.hand:
+                self.hand.remove(tile)
+            else:
+                raise Exception
