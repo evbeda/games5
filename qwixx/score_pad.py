85fedfe9e79e7cde3eee108799bc4ca59bf071da
vgatica-eb <vgatica@eventbrite.com>
2021-03-17 12:28:22 -0300

    adding testing for score

--- qwixx/score_pad.py
+++ qwixx/score_pad.py
@@ -17,7 +17,32 @@ class ScorePad:
         if self.penalty < 4:
             return self.penalty
         else:
-            return True
+            return True  # terminar juego
 
-    def calculate_score(self):
-        raise NotImplementedError
+    def calculate_marks(self):
+        dict_marks = {
+            1: 1,
+            2: 3,
+            3: 6,
+            4: 10,
+            5: 15,
+            6: 21,
+            7: 28,
+            8: 36,
+            9: 45,
+            10: 55,
+            11: 66,
+            12: 78,
+        }
+        for i in self.rows:
+            m = len(i.marks)
+            subt = dict_marks(m)
+            score += subt
+        return score
+
+
+     def calculate_score(self):
+        score = calculate_marks()
+        penalty =self.penalty * 5
+
+        return score - penalty
