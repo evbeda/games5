33055e8ea9b2c455aeb5fbecd0f49418542489c1
brz <ibrizuela@eventbrite.com>
2021-03-17 14:10:31 -0300

    testing and class, spot and board

--- scrabble/game/spot.py
+++ scrabble/game/spot.py
@@ -1,3 +1,12 @@
+
+    
 class Spot:
-    def __init__(self, Tile):
-        self.tile = Tile
+    def __init__(self, mult_value, mult_type):
+        self.tile = None
+        self.mult_value = mult_value
+        self.mult_type = mult_type
+
+    def set_tile(self, tile):
+        self.tile = tile
+    
+
