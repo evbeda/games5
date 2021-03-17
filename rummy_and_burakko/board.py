642b0c87aa541de121dd5111e73debe9e21b9479
srobledo-eb <srobledo@eventbrite.com>
2021-03-17 18:15:14 -0300

    test correct creation of sets into board

--- rummy_and_burakko/board.py
+++ rummy_and_burakko/board.py
@@ -3,5 +3,15 @@ class Board():
         self.sets = []
 
     def add_new_play(self, sets):
+        if self.validate_sets(sets):
+            for item in sets:
+                self.sets.append(item)
+            return True
+        else:
+            return False
+
+    def validate_sets(self, sets):
         for item in sets:
-            self.sets.append(item)
+            if not(item.is_valid()):
+                return False
+        return True
--- rummy_and_burakko/tests/test_board.py
+++ rummy_and_burakko/tests/test_board.py
@@ -1,17 +1,35 @@
 import unittest
+from unittest.mock import patch
 from ..board import Board
 from ..set_tiles import SetTiles
+from parameterized import parameterized
 
 
 class TestBoard(unittest.TestCase):
-    def test_add_new_play(self):
+    @patch.object(Board, 'validate_sets', return_value=True)
+    def test_add_new_play(self, mock_validate_sets):
         board = Board()
         set_one = SetTiles([('red', 5), ('red', 6), ('red', 7)], 1)
         set_two = SetTiles([('blue', 5), ('blue', 6), ('blue', 7)], 2)
-        sets = []
-        sets.append(set_one)
-        sets.append(set_two)
-        board.add_new_play(sets)
+        board.add_new_play([set_one, set_two])
 
         self.assertEqual(board.sets[0], set_one)
         self.assertEqual(board.sets[1], set_two)
+
+    @parameterized.expand([
+        (
+            [SetTiles([('red', 5), ('red', 6), ('red', 8)], 1),
+                SetTiles([('blue', 5), ('blue', 6), ('blue', 7)], 2)],
+            False
+        ),
+        (
+            [SetTiles([('red', 5), ('red', 6), ('blue', 7)], 1),
+                SetTiles([('blue', 5), ('blue', 6), ('blue', 7)], 2)],
+            False
+        ),
+    ])
+    @patch.object(SetTiles, 'is_valid', return_value=False)
+    def test_validate_sets(self, sets, expected, mock_validate_sets):
+        board = Board()
+        board.validate_sets(sets)
+        self.assertEqual(board.add_new_play(sets), expected)
