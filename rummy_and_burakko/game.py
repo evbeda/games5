2f4eaaa98ab3426db9b41b1a94bb0ab85847e342
Andres Bonilla <abonilla@eventbrite.com>
2021-03-17 14:31:24 -0300

    Add player creation

--- rummy_and_burakko/game.py
+++ rummy_and_burakko/game.py
@@ -1,6 +1,12 @@
+from .player import Player
+
+
 class Game:
 
     def __init__(self):
         self.remaining_tiles = 106
         self.players = []
         self.current_turn = 0
+
+    def create_players(self, names):
+        self.players = [Player(name) for name in names]
--- rummy_and_burakko/tests/test_game.py
+++ rummy_and_burakko/tests/test_game.py
@@ -1,9 +1,13 @@
 import unittest
+from unittest.mock import patch
 
 from ..game import Game
 
 
 class TestGame(unittest.TestCase):
+
+    def setUp(self):
+        self.game = Game()
 
     def test_game_attributes(self):
         game = Game()
@@ -11,3 +15,12 @@ class TestGame(unittest.TestCase):
         self.assertEqual(game.remaining_tiles, 106)
         self.assertEqual(game.players, [])
         self.assertEqual(game.current_turn, 0)
+
+    def test_create_players(self):
+        player_names = ["Pedro", "Juana", "Mia"]
+        
+        with patch('rummy_and_burakko.game.Player') as player_patched:
+            self.game.create_players(player_names)
+
+        self.assertEqual(len(self.game.players), 3)
+        player_patched.assert_called()
