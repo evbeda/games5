4be7be8066d6ef2aef7b36e3002145641974858c
Enzo Crespillo <ecrespillo@eventbrite.com>
2021-03-17 11:58:00 -0300

    reworking dice and set_dice tests

--- qwixx/test_set_dices.py
+++ qwixx/test_set_dices.py
@@ -7,10 +7,21 @@ class TestSetDices(unittest.TestCase):
     def setUp(self):
         self.t_set_dices = SetDices()
 
-    @patch('random.randint')
+    @patch('random.randint', return_value=3)
     def test_roll_dices(self, random_mock):
+        # data
+        values_test = {
+            'white_1': 3,
+            'white_2': 3,
+            'red': 3,
+            'yellow': 3,
+            'blue': 3,
+            'green': 3
+        }
         # process
         values = self.t_set_dices.roll_dices()
 
         # assert
         self.assertEqual(random_mock.call_count, 6)
+        self.assertEqual(len(values), 6)
+        self.assertEqual(values, values_test)
