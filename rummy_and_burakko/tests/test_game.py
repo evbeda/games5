1e0d1c9ace9e9a993aaa2ad1efc72224165d7c29
vgatica-eb <vgatica@eventbrite.com>
2021-03-18 16:10:05 -0300

    adding test of assign tiles

--- rummy_and_burakko/tests/test_game.py
+++ rummy_and_burakko/tests/test_game.py
@@ -1,8 +1,10 @@
 import unittest
 from unittest.mock import patch
 from parameterized import parameterized
+from unittest import mock
 
 from ..game import Game
+from ..game import Player
 
 
 class TestGame(unittest.TestCase):
@@ -19,7 +21,7 @@ class TestGame(unittest.TestCase):
 
     def test_create_players(self):
         player_names = ["Pedro", "Juana", "Mia"]
-        
+
         with patch('rummy_and_burakko.game.Player') as player_patched:
             self.game.create_players(player_names)
 
@@ -39,7 +41,7 @@ class TestGame(unittest.TestCase):
         self.game.next_turn()
 
         self.assertEqual(self.game.current_turn, next_turn)
-        
+
     def test_create_tiles(self):
         with patch('rummy_and_burakko.game.Tile') as tile_patched:
             self.game.create_tiles()
@@ -47,10 +49,10 @@ class TestGame(unittest.TestCase):
         self.assertEqual(len(self.game.remaining_tiles), 106)
 
         tile_list = {
-            'r': list(range(1,14)),
-            'y': list(range(1,14)),
-            'w': list(range(1,14)),
-            'b': list(range(1,14)),
+            'r': list(range(1, 14)),
+            'y': list(range(1, 14)),
+            'w': list(range(1, 14)),
+            'b': list(range(1, 14)),
             '*': [0] * 2,
         }
 
@@ -65,3 +67,11 @@ class TestGame(unittest.TestCase):
     def test_player_limit(self, players):
         with self.assertRaises(Exception):
             self.game.create_players(players)
+
+    # teste de mock
+    @mock.patch.object(Player, "add_tiles")
+    def test_call_add_tiles_by_asign_tiles(self, mock):
+        t = Game()
+        t.players = [Player('juan'), Player('pedro')]
+        t.asign_tiles()
+        mock.assert_called()
