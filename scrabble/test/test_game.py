757a1c1f62d4fe2236a33d57655c414d7c59df29
Enzo Crespillo <ecrespillo@eventbrite.com>
2021-03-18 17:05:36 -0300

    creating game

--- scrabble/test/test_game.py
+++ scrabble/test/test_game.py
@@ -1,18 +1,42 @@
 import unittest
+from unittest.mock import patch
 from ..game.game import Game
+from ..game.player import Player
+from ..game.tile import Tile
 
 
 class TestGame(unittest.TestCase):
+    def setUp(self):
+        players = ["player_1", "player_2", "player_3"]
+        self.t_game = Game(players)
 
+    # actualizar cuando se tenga el manejo de turnos
     def test_game_initiator(self):
-        g = Game(3)
-        self.assertEqual(g.turn, 0)
+        self.assertEqual(len(self.t_game.tile_bag.tiles), 100)
 
-    def test_new_player(self):
-        g = Game(3)
-        self.assertEqual(g.players, 3)
+    def test_create_players(self):
+        self.assertEqual(len(self.t_game.players), 3)
+        self.assertEqual(self.t_game.players[2].name, "player_3")
 
-    def test_new_player_max(self):
+    def test_players_max(self):
         with self.assertRaises(Exception):
             Game(5)
 
+    # class DrawGenerator:
+    #     letters = 'cas'
+
+    #     def __init__(self):
+    #         self.last_index = -1
+
+    #     def __call__(self, *args, **kwargs):
+    #         self.last_index += 1
+    #         return self.letters[self.last_index]
+
+    # draw_generator = DrawGenerator()
+    # def test_first_player(self):
+    #     with patch.object(Player, 'one_draw', side_effect='draw_generator') as mock_one_draw:
+    #         players = ["player_1"]
+    #         t_game = Game(players)
+    #         x = t_game.first_player()
+    #         self.assertEqual(mock_one_draw.call_count, 3)
+    #         self.assertEqual(x, 0)
