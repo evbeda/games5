1ea1c22b4287a35a8f2fc5001a0c210bb983581f
Andres Bonilla <abonilla@eventbrite.com>
2021-03-18 14:47:32 -0300

    Allow reusing board pieces in new sets

--- rummy_and_burakko/tests/test_board.py
+++ rummy_and_burakko/tests/test_board.py
@@ -2,30 +2,32 @@ import unittest
 from unittest.mock import patch
 from ..board import Board
 from ..set_tiles import SetTiles
+from ..tile import Tile
 from parameterized import parameterized
 
 
 class TestBoard(unittest.TestCase):
+    @patch('rummy_and_burakko.game.Tile', create=True)
     @patch.object(Board, 'validate_sets', return_value=True)
-    def test_add_new_play(self, mock_validate_sets):
+    def test_add_new_play(self, mock_validate_sets, tile_patched):
         board = Board()
-        set_one = SetTiles([('red', 5), ('red', 6), ('red', 7)], 1)
-        set_two = SetTiles([('blue', 5), ('blue', 6), ('blue', 7)], 2)
+        set_one = SetTiles([Tile('r', 5), Tile('r', 6), Tile('r', 7)], 1)
+        set_two = SetTiles([Tile('b', 5), Tile('b', 6), Tile('b', 7)], 2)
         board.add_new_play([set_one, set_two])
 
-        self.assertEqual(board.sets[0], set_one)
-        self.assertEqual(board.sets[1], set_two)
+        self.assertIn(set_one, board.sets)
+        self.assertIn(set_two, board.sets)
 
     @parameterized.expand([
         (
-            [SetTiles([('red', 5), ('red', 6), ('red', 8)], 1),
-                SetTiles([('blue', 5), ('blue', 6), ('blue', 7)], 2)],
+            [SetTiles([Tile('red', 5), Tile('red', 6), Tile('red', 8)], 1),
+                SetTiles([Tile('blue', 5), Tile('blue', 6), Tile('blue', 7)], 2)],
             False
         ),
         (
-            [SetTiles([('red', 5), ('red', 6), ('blue', 7)], 1),
-                SetTiles([('blue', 5), ('blue', 6), ('blue', 7)], 2)],
-            True
+            [SetTiles([Tile('red', 5), Tile('red', 6), Tile('blue', 7)], 1),
+                SetTiles([Tile('blue', 5), Tile('blue', 6), Tile('blue', 7)], 2)],
+            False
         ),
     ])
     def test_validate_sets(self, sets, expected):
@@ -35,3 +37,18 @@ class TestBoard(unittest.TestCase):
             board.validate_sets(sets)
         
         self.assertEqual(board.add_new_play(sets), expected)
+
+    @patch.object(Board, 'validate_sets', return_value=True)
+    def test_use_tiles_in_board(self, board_patched):
+        board = Board()
+
+        with patch('rummy_and_burakko.game.Tile', create=True):
+            (t1, t2, t3, t4, t5, t6) = [Tile('*', 0) for _ in range(6)]
+            [t.assign_set_id(1) for t in (t1, t2, t3, t4)]
+            set_1 = SetTiles([t1, t2, t3, t4], 1),
+            board.sets = [set_1]
+            set_2 = SetTiles([t2, t5, t6], 2)
+            board.add_new_play([set_2])
+        
+        self.assertIn(set_2, board.sets)
+        self.assertNotIn(set_1, board.sets)
