2abe5570883c5b99c2ecb5ac560a1779698c06fc
srobledo-eb <srobledo@eventbrite.com>
2021-03-18 11:31:08 -0300

    modification over test_can_mark to include the is_locked condition

--- qwixx/row.py
+++ qwixx/row.py
@@ -2,16 +2,18 @@ class Row:
     def __init__(self, color):
         self.color = color
         self.numbers = self.create_row_numbers()
-        self.state = False
+        self.is_locked = False
         self.marks = []
 
     def create_row_numbers(self):
-        return (tuple(range(2, 13))
-            if self.color in ['red', 'yellow']
-            else tuple(reversed(range(2, 13))))
+        return (
+                tuple(range(2, 13))
+                if self.color in ['red', 'yellow']
+                else tuple(reversed(range(2, 13)))
+            )
 
     def lock_Row(self):
-        self.state = True
+        self.is_locked = True
         return True
 
     def can_mark_last(self):
@@ -22,9 +24,18 @@ class Row:
             self.marks.append(number)
 
     def can_mark(self, number):
-        if (number > self.marks[-1]) and (self.color in ['red', 'yellow']):
+
+        if (
+            (number > self.marks[-1])
+            and (self.color in ['red', 'yellow'])
+            and (not self.is_locked)
+        ):
             return True
-        elif (number < self.marks[-1]) and (self.color in ['blue', 'green']):
+        elif (
+            (number < self.marks[-1])
+            and (self.color in ['blue', 'green'])
+            and (not self.is_locked)
+        ):
             return True
         else:
             return False
