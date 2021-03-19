e904c09e457455e623acc8ba41a124e24a329491
Andres Bonilla <abonilla@eventbrite.com>
2021-03-19 11:00:36 -0300

    Add test for spot formatting

--- scrabble/test/test_spot.py
+++ scrabble/test/test_spot.py
@@ -15,3 +15,13 @@ class TestSpot(unittest.TestCase):
         s = Spot(0,'c')
         s.set_tile(t)
         self.assertEqual(s.tile.letter, t.letter)
+
+    @parameterized.expand([
+        (Spot(0, 'c'), Tile('a'), ' a '),
+        (Spot(2, 'l'), None, 'x2l'),
+    ])
+    def test_spot_format(self, spot, tile, expected):
+        s = spot
+        if tile:
+            s.set_tile(tile)
+        self.assertEqual(s.get_spot(), expected)
