9681bd44d16db12eceb8d348361df0e7aec21717
srobledo-eb <srobledo@eventbrite.com>
2021-03-19 11:22:51 -0300

    polishing code

--- rummy_and_burakko/game.py
+++ rummy_and_burakko/game.py
@@ -1,13 +1,10 @@
-# import random
 from .player import Player
-# from .tile import Tile
 from .tile_bag import TileBag
 
 
 class Game:
 
     def __init__(self):
-        # self.remaining_tiles = []
         self.players = []
         self.current_turn = 0
         self.tile_bag = TileBag()
@@ -23,17 +20,3 @@ class Game:
 
     def distribute_tiles(self):
         self.tile_bag.assign_tiles(self.players)
-            
-
-    # def create_tiles(self):
-    #     self.remaining_tiles = list(itertools.chain(
-    #         *[[Tile(color, number) for color in ['b', 'r', 'y', 'w']*2] for
-    #             number in range(1, 14)]
-    #     ))
-    #     self.remaining_tiles += [Tile('*', 0), Tile('*', 0)]
-
-    # def asign_tiles(self):
-    #     random.shuffle(self.remaining_tiles)
-    #     for i in self.players:
-    #         i.add_tiles(self.remaining_tiles[:14])
-    #         self.remaining_tiles = self.remaining_tiles[15:]
