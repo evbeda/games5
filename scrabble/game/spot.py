3e28a3a9b19231734f7fa11ccb51c3231cc40f05
Andres Bonilla <abonilla@eventbrite.com>
2021-03-19 11:14:22 -0300

    Add spot formatting

--- scrabble/game/spot.py
+++ scrabble/game/spot.py
@@ -6,3 +6,6 @@ class Spot:
 
     def set_tile(self, tile):
         self.tile = tile
+
+    def get_spot(self):
+        return f' {self.tile.letter} ' if self.tile else f'x{self.mult_value}{self.mult_type}' if self.mult_value else '   '
