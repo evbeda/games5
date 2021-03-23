e7d0469a41eaca4050a107f8dd9123fdc457bde0
srobledo-eb <srobledo@eventbrite.com>
2021-03-23 15:48:07 -0300

    fix flake8 issues in board

--- rummy_and_burakko/board.py
+++ rummy_and_burakko/board.py
@@ -48,3 +48,11 @@ class Board():
 
     def validate_turn(self):
         self.sets = self.temp_sets.copy()
+
+    def give_one_tile(self, player, set_id, index):
+        if len(self.sets[set_id]) > 0:
+            player.add_tiles(
+                    [self.sets[set_id].extract_one_tile(index)]
+                )
+        else:
+            raise Exception
