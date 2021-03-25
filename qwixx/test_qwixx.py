addcadca03617d77318d1078824560c80ae2b04d
srobledo-eb <srobledo@eventbrite.com>
2021-03-25 12:40:05 -0300

    Removed method that creates SetDices so now it is carried out in the constructor of class Qwixx

--- qwixx/test_qwixx.py
+++ qwixx/test_qwixx.py
@@ -1,34 +1,11 @@
 import unittest
-from unittest.mock import patch, Mock
+from unittest.mock import Mock
 from .qwixx import Qwixx
-from .score_pad import ScorePad
 
 
 class TestQwixx(unittest.TestCase):
     def setUp(self):
         self.qwixx = Qwixx()
-
-    @unittest.skip('raise NotImplementedError in create_dice_set')
-    def test_dice_set(self):
-        expected_color_amount = {
-            'white': 2,
-            'red': 1,
-            'yellow': 1,
-            'green': 1,
-            'blue': 1,
-        }
-        result_color_amount = {}
-        self.qwixx.create_dice_set()
-
-        for dice in self.qwixx.dice_set:
-            dice_color_amount = result_color_amount.setdefault(dice.color, 0)
-            dice_color_amount += 1
-
-        for expected_color, expected_amount in expected_color_amount.items():
-            self.assertEqual(
-                result_color_amount[expected_color],
-                expected_amount
-            )
 
     def test_remove_dice_from_set(self):
         self.qwixx.dice_set = []
