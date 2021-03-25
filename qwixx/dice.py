f904855f4cb1328693362b499aceea3c8edec86f
srobledo-eb <srobledo@eventbrite.com>
2021-03-25 14:36:13 -0300

    Add att value to die

--- qwixx/dice.py
+++ qwixx/dice.py
@@ -4,6 +4,8 @@ import random
 class Dice:
     def __init__(self, color):
         self.color = color
+        self.value = 0
 
     def roll_dice(self):
-        return random.randint(1, 6)
+        # return random.randint(1, 6)
+        self.value = random.randint(1, 6)
