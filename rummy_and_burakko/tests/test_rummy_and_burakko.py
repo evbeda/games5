a0865e8fb39e8b4b97c1d598bb39cdaf3a7e7c7b
Enzo Crespillo <ecrespillo@eventbrite.com>
2021-03-25 18:07:39 -0300

    first tests interface

--- rummy_and_burakko/tests/test_rummy_and_burakko.py
+++ rummy_and_burakko/tests/test_rummy_and_burakko.py
@@ -1,12 +1,33 @@
 import unittest
-from ..rummy_and_burakko import RummyAndBurakko
+from unittest.mock import patch
 from parameterized import parameterized
+from ..rummy_and_burakko import RummyAndBurakko
+from ..game import Game
 
 
 class TestRummyAndBurakko(unittest.TestCase):
     def setUp(self):
         self.rummy = RummyAndBurakko()
 
-    # def test_init(self):
-    #     x = self.rummy.probando()
-    #     self.assertEqual(x, "anda")
+    def test_constructor(self):
+        pass
+
+    @patch.object(Game, "show_game", return_value="test")
+    def test_board(self, mock):
+        result = self.rummy.board
+        self.assertEqual(result, "Starting...")
+
+        self.rummy.game = Game()
+        result = self.rummy.board
+        mock.assert_called_once_with()
+        self.assertEqual(mock.return_value, "test")
+
+    @parameterized.expand([
+        # (input_players)
+        (0, 0, 'start_game'),
+        (1, 1, 'players_input'),
+    ])
+    def test_play_start_game(self, input_players, res_1, res_2):
+        self.rummy.play_start_game(input_players)
+        self.assertEqual(self.rummy.input_player_args, res_1)
+        self.assertEqual(self.rummy.game_state, res_2)
