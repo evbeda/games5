a613e39fbd8a3cfe95e0938dae6d52299731211f
brz <ibrizuela@eventbrite.com>
2021-03-26 15:06:59 -0300

    Adding test_eq in Tile

--- rummy_and_burakko/tests/test_tile.py
+++ rummy_and_burakko/tests/test_tile.py
@@ -22,3 +22,10 @@ class TestTiles(unittest.TestCase):
         tile = Tile('blue', 2)
         tile.assign_set_id(30)
         self.assertEqual(tile.set_id, 30)
+
+    @parameterized.expand([
+        (Tile('red', 5), Tile('red', 5), True),
+        (Tile('red', 5), Tile('blue', 2), False),
+    ])
+    def test_eq(self, t1, t2, expected):
+        self.assertEqual(t1 == t2, expected)
