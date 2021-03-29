888eecf2d7636a899813ee2f508081b746c09ece
Andres Bonilla <abonilla@eventbrite.com>
2021-03-29 17:31:40 -0300

    Fix place word and tile bag trailing comma

--- scrabble/tile.py
+++ scrabble/tile.py
@@ -1,26 +1,22 @@
 class Tile:
 
-    set_tile = (('aeoisnlrut', 1),
-                ('dg', 2),
-                ('cbmp', 3),
-                ('hfvy', 4),
-                ('ch-q'.split('-'), 5),
-                ('j-ll-ñ-rr-x'.split('-'), 8),
-                ('z', 10)
-                )
+    set_tile = (
+        ('aeoisnlrut', 1),
+        ('dg', 2),
+        ('cbmp', 3),
+        ('hfvy', 4),
+        ('ch-q'.split('-'), 5),
+        ('j-ll-ñ-rr-x'.split('-'), 8),
+        ('z', 10)
+    )
 
     def __init__(self, letter):
         self.letter = letter
         self.score = 0
-        for t in Tile.set_tile:
-            if letter in t[0]:
-                self.score = t[1]
+        for tile_set, score in Tile.set_tile:
+            if letter in tile_set:
+                self.score = score
                 break
-
-    @property
-    def order(self):
-
-        return ord(self.letter[0]) - ord('a')
 
     def __eq__(self, other):
         return self.letter == other.letter
