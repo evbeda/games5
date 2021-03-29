a61796a3e64f44d85bda1a43a8d0db13b9a8ab36
Enzo Crespillo <ecrespillo@eventbrite.com>
2021-03-29 13:19:36 -0300

    board and player get_a_tile

--- rummy_and_burakko/game.py
+++ rummy_and_burakko/game.py
@@ -2,6 +2,7 @@ from .player import Player
 from .tile_bag import TileBag
 from .board import Board
 import random
+from copy import deepcopy
 
 
 class Game:
@@ -38,11 +39,31 @@ class Game:
     def make_play(self, option, args):
         player = self.players[self.current_turn]
         options = {
-            # 1: self.board.put_new_set(player, *args)
+            # 1: self.put_new_set(*args)
             # 2: self.board.put_a_tile(player, *args)
             3: self.board.give_one_tile_from_board(*args)
         }
         options[option]
+
+    def put_new_set(self, indexes):
+        # intercambiar indices por tiles (copiando)
+        tiles = self.make_tile_array(indexes)
+        # eliminar tiles respecto a los indices
+        # crear el set con las tiles
+        # guardar el set
+
+    def make_tile_array(self, indexes):
+        tiles = []
+        player = self.players[self.current_turn]
+        for index in indexes:
+            try:
+                tile = deepcopy(player.get_a_tile(index))
+            except:
+                tile = deepcopy(self.board.get_a_reused_tile(index))
+
+            tiles.append(tile)
+
+        return tiles
 
     def quantity_of_tiles(self):
         return self.players[self.current_turn].get_lenght()
