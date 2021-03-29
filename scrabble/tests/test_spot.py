485eedf4a8fd39afea06ac394badb4d6e9968373
Andres Bonilla <abonilla@eventbrite.com>
2021-03-29 13:13:15 -0300

    Fix linting in test_spot

--- scrabble/tests/test_spot.py
+++ scrabble/tests/test_spot.py
@@ -7,13 +7,13 @@ from ..tile import Tile
 class TestSpot(unittest.TestCase):
 
     def test_spot_constructor(self):
-        s = Spot(0,'c')
+        s = Spot(0, 'c')
         self.assertEqual(s.mult_value, 0)
         self.assertEqual(s.mult_type, 'c')
 
     def test_set_tile(self):
         t = Tile('a')
-        s = Spot(0,'c')
+        s = Spot(0, 'c')
         s.set_tile(t)
         self.assertEqual(s.tile.letter, t.letter)
 
