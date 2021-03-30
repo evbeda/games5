a9f19b55c743b34b9d30370336f6c4bb053e9b8c
Andres Bonilla <abonilla@eventbrite.com>
2021-03-30 11:21:19 -0300

    Parameterize multiply score test

--- scrabble/tests/test_score.py
+++ scrabble/tests/test_score.py
@@ -1,6 +1,7 @@
 from ..score import Score
 from ..spot import Spot
 import unittest
+from parameterized import parameterized
 
 
 class TestScore(unittest.TestCase):
@@ -14,8 +15,16 @@ class TestScore(unittest.TestCase):
     # def test_search_vert_word(self):
     #     pass
 
-    def test_multiply_score(self):
-        spots = [Spot(0, 'c')]
-        word = 'a'
+    @parameterized.expand([
+        (
+            [
+                (0, 'c'),
+            ],
+            'a',
+            1,
+        ),
+    ])
+    def test_multiply_score(self, spot_values, word, expected):
+        spots = [Spot(m_value, m_type) for m_value, m_type in spot_values]
         score = Score.multiply_score(spots, word)
-        self.assertEqual(score, 1)
+        self.assertEqual(score, expected)
