5e8bb11a1c18db967eadbb93d72b6946818afc03
Andres Bonilla <abonilla@eventbrite.com>
2021-03-30 11:01:47 -0300

    Calculate score for word in spots

--- scrabble/score.py
+++ scrabble/score.py
@@ -1,13 +1,27 @@
-# class Score:
+from .tile import Tile
 
-#     def score_word(self):
-#         pass
 
-#     def search_horiz_word(self):
-#         pass
+class Score:
 
-#     def search_vert_word(self):
-#         pass
+    # def score_word(self):
+    #     pass
 
-#     def multiply_score(self):
-#         pass
+    # def search_horiz_word(self):
+    #     pass
+
+    # def search_vert_word(self):
+    #     pass
+
+    def multiply_score(self, spots, word):
+        score = 0
+        word_mult = 1
+        for spot, letter in zip(spots, word):
+            if spot.tile is None and spot.mult_type == 'w':
+                word_mult *= spot.mult_value
+
+            if spot.tile is None and spot.mult_type == 'l':
+                score += Tile(letter).score * spot.mult_value
+            else:
+                score += Tile(letter).score
+
+        return score * word_mult
