584ac41dbe112bd7f327427b1f98a570c678b2e5
Andres Bonilla <abonilla@eventbrite.com>
2021-03-30 16:46:05 -0300

    Change search method name

--- scrabble/tests/test_score.py
+++ scrabble/tests/test_score.py
@@ -170,6 +170,6 @@ class TestScore(unittest.TestCase):
                 word.append(spot)
             words.append(word)
 
-        with patch.object(Score, 'search_words', return_value=words):
+        with patch.object(Score, 'define_direction', return_value=words):
             score = Score.get_score('word', 'col', 'row', 'direction', 'spots')
             self.assertEqual(score, expected)
