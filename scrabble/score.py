1f96efde8e6bf7d8046267d5dbb6cf3aec949e41
vgatica-eb <vgatica@eventbrite.com>
2021-03-30 12:01:14 -0300

    wrtiting method

--- scrabble/score.py
+++ scrabble/score.py
@@ -2,6 +2,33 @@ class Score:
 
     # def score_word(self):
     #     pass
+class Score:
+    #     def score_word(self):
+    #         pass
+    # para palabras horizontales, hacer verificacion horizontal para q no toque letra despues
+    # para palabras verticales, hacer verificacion vertical para q no toque otra letra despues
+
+    # mi palabra original es vertical
+    @staticmethod
+    def search_horiz_word(self, word, row, col):
+        len_word = len(word)
+        end_word = row + len_word
+        for i in range(row, end_word):
+            if [i][col - 1] == 'algo' or [i][col + 1] == 'algo':
+                # recorrer para izquierda
+                # luego recorrer para derecha
+                pass
+
+    # mi palabra original es horizontal
+    @staticmethod
+    def search_vert_word(self, word, row, col):
+        len_word = len(word)
+        end_word = row + len_word
+        for i in range(row, end_word):
+            if [i][col - 1] == 'algo' or [i][col + 1] == 'algo':
+                # recorrer para izquierda
+                # luego recorrer para derecha
+                pass
 
     # def search_horiz_word(self):
     #     pass
